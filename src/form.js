d4a191c1c635553ee23e573115abcfb32c5af320
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-06-29 14:19:48 +0300

    Testing jsdoc

--- src/form.js
+++ src/form.js
@@ -293,19 +293,19 @@ define(["jquery", "./client", "./meta"], function($, $client, $meta) {
 	};
 
 	/**
-	 * @callback initForm~options~successCallback
+	 * @callback form.initForm~options~successCallback
 	 * @param {object} form - HTML element
 	 */
 
 	/**
-	 * @typedef initForm~options
+	 * @typedef form.initForm~options
 	 * @type {object}
-	 * @property {initForm~options~successCallback} success - Callback which will be called after the form has been built.
+	 * @property {form.initForm~options~successCallback} success - Callback which will be called after the form has been built.
 	 */
 
 	/** Initialize forms 
 	 * @param {object|string} obj - Element where the form will be created. It can also be JQuery selector string.
-	 * @param {initForm~options} opts - Options for initialization
+	 * @param {form.initForm~options} opts - Options for initialization
 	 */
 	exports.initForm = function(obj, opts) {
 			opts = opts || {};
