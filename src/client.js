7772cbc3dbfe36a7236d8569c12f54b7232230cd
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-07-01 12:59:43 +0300

    Fixed jsdocs

--- src/client.js
+++ src/client.js
@@ -12,7 +12,7 @@ define(["jquery"], function ($) {
 	var exports = {};
 
 	/** Populate default values from ._meta.defaults if it exists 
-	 * @memberof exports
+	 * @memberof module:nor-ui/client
 	 * @inner
 	 * @method populate_defaults sets default values if they do not exist
 	 * @param defs default meta object
@@ -32,7 +32,7 @@ define(["jquery"], function ($) {
 	}
 
 	/** Fetch resource 
-	 * @memberof exports 
+	 * @memberof module:nor-ui/client 
 	 * @param url
 	 * @param fn function callback
 	 * @TODO $.getJSON could be implemented using common API which supports multiple implementations for HTTP requests, so that jquery would not be a dependency.
