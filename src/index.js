509631c10be4468b24b08bf3a46c690a81813b04
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-07-01 13:17:09 +0300

    Fixed jsdocs namespaces

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ define(["./form", "./client", "./meta", "./modal"], function(form, client, meta,
 	/** 
 	 * The main include path for our public API
 	 * @module nor-ui
-	 * @borrows form as module:nor-ui/form
-	 * @borrows client as module:nor-ui/client
-	 * @borrows meta as module:nor-ui/meta
-	 * @borrows modal as module:nor-ui/modal
+	 * @borrows form as nor-ui.form
+	 * @borrows client as nor-ui.client
+	 * @borrows meta as nr-ui.meta
+	 * @borrows modal as nor-ui.modal
 	 */
 	var exports = {
 		'form':   form,
