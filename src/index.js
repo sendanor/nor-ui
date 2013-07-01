08a8e0cc827a2b53b375846488c0a8434b73694f
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-07-01 10:22:47 +0300

    Testing @borrows

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ if (typeof define !== 'function') {
     var define = require('amdefine')(module);
 }
 define(["./form", "./client", "./meta", "./modal"], function(form, client, meta, modal) {
-   /** 
-    * The main include path for our public API
-    * @exports nor-ui
-    * @version 0.1.0
-    */
+	/** 
+	 * The main include path for our public API
+	 * @exports nor-ui
+	 * @version 0.1.0
+	 * @namespace
+	 * @borrows form as nor-ui/form
+	 * @borrows client as nor-ui/client
+	 * @borrows meta as nor-ui/meta
+	 * @borrows modal as nor-ui/modal
+	 */
 	var index = {
 		'form':   form,
 		'client': client,
